a3a8f2a0e79ad64f9e7acaa5a812fb36d62bcd2d
Chabu Chabu <chabu.chabu@episerver.com>
2023-01-04 00:14:20 +0000

    routing pages staticatly

--- main.go
+++ main.go
@@ -5,12 +5,63 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!!</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1> Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:chabuchabu32@gmail.com\">chabucabu32@gmail.com</a>.")
+
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1> Frequently asked questions</h1>
+	 <ul>
+	   <li>is there a free version for this ? Yes! we offer a trial of 30 days for any paid plan.</li>
+	 </ul>
+	 <ul>
+	   <li> Are there any prerequisites ? Absolutely not! we explain everything from ground up.</li>
+     </ul>
+     `)
+}
+
+// func pathhandler(w http.ResponseWriter, r *http.Request) {
+
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		//handle the page not found error
+// 		http.Error(w, "page not found 3", http.StatusNotFound)
+
+// 	}
+// }
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		//handle the page not found error
+		http.Error(w, "page not found 3", http.StatusNotFound)
+
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	var router Router
 	fmt.Println("Starting the server on port:3000....")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
