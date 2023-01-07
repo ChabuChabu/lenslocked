66e121003459d60df0157e32fe0e0cf8f3caa9be
Chabu Chabu <chabu.chabu@episerver.com>
2023-01-07 23:23:22 +0000

    contact page templete added

--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	// "golang.org/x/tools/go/analysis/passes/nilfunc"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html;charset=utf-8")
-	tpl, err := template.ParseFiles("templates/home.gohtml")
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
 		log.Printf("Parsing templete: %v", err)
 		http.Error(w, "There is an error parsing the template.", http.StatusInternalServerError)
@@ -26,9 +27,14 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1> Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:chabuchabu32@gmail.com\">chabucabu32@gmail.com</a>.")
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 
 }
 
